4bbce82ed7e0f2d539ad60ff495822ad587c021e
gringrape <gringrape200@gmail.com>
2023-02-24 01:57:23 +0900

    오디션 지원 및 지원확인 API

--- src/server.js
+++ src/server.js
@@ -31,6 +31,57 @@ server.get('/auditions', (req, res) => {
   )));
 });
 
+server.patch('/auditions', async (req, res) => {
+  const { auditionTitle } = req.query;
+
+  if (!auditionTitle) {
+    res.status(404).send();
+    return;
+  }
+
+  const { auditions } = db.data;
+
+  if (!auditions.some((audition) => audition.title === auditionTitle)) {
+    res.status(404).send();
+    return;
+  }
+
+  const audition = auditions.find((a) => a.title === auditionTitle);
+
+  if (!audition.appliedAuditionees.some(({ email }) => email === 'zoonyfil@nate.com')) {
+    audition.appliedAuditionees.push({
+      name: '이승찬',
+      gender: 'male',
+      age: '29',
+      email: 'zoonyfil@nate.com',
+      height: '176',
+      weight: '72',
+      imgSrc: 'https://findactor.s3.ap-northeast-2.amazonaws.com/2023-02-16T08%3A03%3A43.810Z-2.jpg',
+    });
+  }
+
+  await db.write();
+
+  res.status(200).send();
+});
+
+server.get('/auditions/applied', (req, res) => {
+  const { auditionTitle } = req.query;
+
+  if (!auditionTitle) {
+    res.status(404).send();
+    return;
+  }
+
+  const { auditions } = db.data;
+
+  const audition = auditions.find((a) => a.title === auditionTitle);
+
+  const isApplied = audition.appliedAuditionees.some(({ email }) => email === 'zoonyfil@nate.com');
+
+  res.send(isApplied);
+});
+
 server.post('/auditions', async (req, res) => {
   const auditionData = req.body;
 
